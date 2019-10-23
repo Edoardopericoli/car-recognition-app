0e9dd84068fe710e587aa1d376a42bd6a2d36837
edoardopericoli <pericoli.edoardo@gmail.com>
2019-10-23 12:13:27 +0200

    Finished structure of main file (no tried yet)

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from keras import regularizers, optimizers
 import tensorflow as tf
 import keras
 from matplotlib import pyplot as plt
+import csv
 from math import ceil
 import click
 import logging
@@ -34,7 +35,6 @@ def main(initial_parameters_path):
     logging.info('Loading data')
     train_df = pd.read_csv("data/labels/train_labels.csv")
     validation_df = pd.read_csv("data/labels/validation_labels.csv")
-    test_df = pd.read_csv("data/labels/test_labels.csv")
 
     with open(initial_parameters_path) as file:
         initial_parameters = yaml.load(file)
@@ -43,12 +43,10 @@ def main(initial_parameters_path):
     # Asserting dimensions of batch sizes
     asserting_batch_size(length_data=len(train_df), batch_size=initial_parameters['train_batch_size'])
     asserting_batch_size(length_data=len(validation_df), batch_size=initial_parameters['validation_batch_size'])
-    asserting_batch_size(length_data=len(test_df), batch_size=initial_parameters['test_batch_size'])
 
     logging.info('Transforming data using ImageDataGenerator')
     train_image_generator = ImageDataGenerator(rescale=1./255)
     validation_image_generator = ImageDataGenerator(rescale=1./255)
-    test_image_generator = ImageDataGenerator(rescale=1./255)
 
     train_generator = train_image_generator.flow_from_dataframe(
         dataframe=train_df,
@@ -74,27 +72,15 @@ def main(initial_parameters_path):
         target_size=(initial_parameters['IMG_HEIGHT'], initial_parameters['IMG_WIDTH'])
     )
 
-    test_generator = test_image_generator.flow_from_dataframe(
-        dataframe=test_df,
-        directory="data/test",
-        x_col="fname",
-        y_col=initial_parameters['classes'],
-        batch_size=initial_parameters['test_batch_size'],
-        seed=initial_parameters['seed'],
-        shuffle=True,
-        class_mode="other",
-        target_size=(initial_parameters['IMG_HEIGHT'], initial_parameters['IMG_WIDTH'])
-    )
-
-    #Model
+    # Model
     model = Sequential([
-        Conv2D(1, 8, activation='relu', input_shape=(initial_parameters['IMG_HEIGHT'], initial_parameters['IMG_WIDTH'], 3)),
+        Conv2D(2, 4, activation='relu', input_shape=(initial_parameters['IMG_HEIGHT'], initial_parameters['IMG_WIDTH'], 3)),
         MaxPooling2D((8, 8)),
         Conv2D(1, 8, padding='same', activation='relu'),
         MaxPooling2D((8, 8)),
         Flatten(),
         Dense(32, activation='relu'),
-        Dense(4, activation='softmax')
+        Dense(len(initial_parameters['classes']), activation='softmax')
     ])
 
     model.compile(optimizer='adam',
@@ -103,50 +89,72 @@ def main(initial_parameters_path):
 
     model.summary()
 
-# TODO: Togliere test dai dataset caricati
 # TODO: Agiungere file per fare evaluation finale
 # TODO: Creare setting per scegliere se fare gridsearch
 
     logging.info('Fitting the model')
     history = model.fit_generator(
-        train_data_gen,
-        epochs=epochs,
-        validation_data=val_data_gen,
-        steps_per_epoch=ceil(83484 / tr_batch_size)
+        train_generator,
+        epochs=initial_parameters['epochs'],
+        validation_data=validation_generator,
+        steps_per_epoch=ceil(len(train_df) / initial_parameters['train_batch_size'])
      )
 
     # Saving model
-    model_names = [name for name in os.listdir(cwd + '/models')]
+    model_names = [name for name in os.listdir('data/models')]
     if len(model_names) == 0:
         model_name = '1'
-        os.mkdir(cwd + '/models/' + model_name)
+        logging.info('Saving: model, initial parameters and architecture into {model_directory}'.format(
+            model_directory='data/models/' + model_name))
+        os.mkdir('data/models/' + model_name)
         yaml_string = model.to_yaml()
-        with open(cwd + '/models/' + model_name + '/architecture.yml', 'w') as outfile:
+        with open('data/models/' + model_name + '/architecture.yml', 'w') as outfile:
             yaml.dump(yaml_string, outfile, default_flow_style=False)
         # Saving global parameters
-        with open(cwd + '/models/' + model_name + '/parameters.yml', 'w') as outfile:
-            yaml.dump(global_parameters, outfile, default_flow_style=False)
+        with open('data/models/' + model_name + '/initial_parameters.yml', 'w') as outfile:
+            yaml.dump(initial_parameters, outfile, default_flow_style=False)
         # Saving estimator
-        #model.save(cwd + '/models/' + model_name + '/model.h5')
+        model.save('data/models/' + model_name + '/model.h5')
     else:
         model_name = str(int(model_names[-1])+1)
-        os.mkdir(cwd + '/models/'+ model_name)
+        logging.info('Saving: model, initial parameters and architecture into {model_directory}'.format(
+            model_directory='data/models/' + model_name))
+        os.mkdir('data/models/' + model_name)
         # Saving architecture
         yaml_string = model.to_yaml()
-        with open(cwd + '/models/' + model_name + '/architecture.yml', 'w') as outfile:
+        with open('data/models/' + model_name + '/architecture.yml', 'w') as outfile:
             yaml.dump(yaml_string, outfile, default_flow_style=False)
         # Saving global parameters
-        with open(cwd + '/models/' + model_name + '/parameters.yml', 'w') as outfile:
-            yaml.dump(global_parameters, outfile, default_flow_style=False)
+        with open('data/models/' + model_name + '/initial_parameters.yml', 'w') as outfile:
+            yaml.dump(initial_parameters, outfile, default_flow_style=False)
         # Saving estimator
-        model.save(cwd + '/models/' + model_name + '/model.h5')
+        model.save('data/models/' + model_name + '/model.h5')
 
-        # Model Performance
-        # acc = history.history['acc']
-        # val_acc = history.history['val_accuracy']
+    # Model Performance
+    train_accuracy = history.history['acc']
+    validation_accuracy = history.history['val_accuracy']
 
-        # loss = history.history['loss']
-        # val_loss = history.history['val_loss']
+    train_loss = history.history['loss']
+    validation_loss = history.history['val_loss']
+
+    rows = zip(train_accuracy, validation_accuracy, train_loss, validation_loss)
+    if len(model_names) == 0:
+        model_name = '1'
+        logging.info('Saving: model performance into {model_directory}'.format(
+            model_directory='data/models/' + model_name))
+        with open('data/models/' + model_name + '/evaluation.csv', 'w') as outfile:
+            writer = csv.writer(outfile)
+            for row in rows:
+                writer.writerow(row)
+    else:
+        model_name = str(int(model_names[-1]) + 1)
+        logging.info('Saving: model performance into {model_directory}'.format(
+            model_directory='data/models/' + model_name))
+        with open('data/models/' + model_name + '/evaluation.csv', 'w') as outfile:
+            writer = csv.writer(outfile)
+            for row in rows:
+                writer.writerow(row)
+
 
 if __name__ == '__main__':
     main()
