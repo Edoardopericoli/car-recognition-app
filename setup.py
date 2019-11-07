682dff7101d35ef8793fa5a3a0ac03bdc87e02f4
edoardomanieri <edoardomanieri@gmail.com>
2019-11-07 23:19:14 +0100

    packaging and refactoring

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='car_prediction',
+                 version='0.1',
+                 description='Car prediction model',
+                 url='https://github.com/Edoardopericoli/Car_Prediction.git',
+                 author=['Edoardo Manieri', 'Edoardo Pericoli', 'Valentina Parietta', 'Martina Cioffi'],
+                 author_email='edoardomanieri@gmail.com',
+                 license='MIT',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False)
