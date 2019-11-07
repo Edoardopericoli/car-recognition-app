8a3efafedb1ced4c828f710be5e1287a75a86e7d
edoardomanieri <edoardomanieri@gmail.com>
2019-11-07 15:37:39 +0100

    added prediction file

--- prediction.py
+++ prediction.py
@@ -13,11 +13,11 @@ import yaml
 @click.command()
 @click.option('--execution_path', default=r"./", help='config model path',
               type=str)
-@click.option('--test_images_path', default=r"./",
+@click.option('--images_path', default=r"./",
               help='path of images (file or directory)', type=str)
-@click.option('--test_labels_path', default=r"./",
+@click.option('--labels_info_path', default=r"./",
               help='path of images (file or directory)', type=str)
-def main(execution_path, test_images_path, test_labels_path):
+def main(execution_path, images_path, labels_info_path):
     # Loading paths and model information
     initial_parameters_path = execution_path + '/initial_parameters.yml'
     with open(initial_parameters_path) as f:
@@ -25,37 +25,34 @@ def main(execution_path, test_images_path, test_labels_path):
     model_path = execution_path + '/model.h5'
     model = load_model(model_path)
 
-    if os.path.isfile(test_images_path):
-        img = cv2.imread(test_images_path)
+    if os.path.isfile(images_path):
+        img = cv2.imread(images_path)
         img = cv2.resize(img, (initial_parameters['IMG_HEIGHT'],
                                initial_parameters['IMG_WIDTH']))
         images = np.reshape(img, [1, initial_parameters['IMG_HEIGHT'],
                                   initial_parameters['IMG_WIDTH'], 3])
 
-        filenames = [os.path.basename(test_images_path)]
+        filenames = [os.path.basename(images_path)]
 
-    elif os.path.isdir(test_images_path):
-        test_images_path = glob.glob(test_images_path + "/*")
-        images = [cv2.imread(f) for f in test_images_path]
+    elif os.path.isdir(images_path):
+        images_path = glob.glob(images_path + "/*")
+        images = [cv2.imread(f) for f in images_path]
         images = np.array([cv2.resize(img,
                                       (initial_parameters['IMG_HEIGHT'],
                                        initial_parameters['IMG_WIDTH']))
                           for img in images])
-        images = np.reshape(images, [len(test_images_path),
+        images = np.reshape(images, [len(images_path),
                                      initial_parameters['IMG_HEIGHT'],
                                      initial_parameters['IMG_WIDTH'], 3])
 
-        filenames = [os.path.basename(img_path)
-                     for img_path in test_images_path]
+        filenames = [os.path.basename(img_path) for img_path in images_path]
     classes = model.predict_classes(images)
 
-    output_df = pd.DataFrame({'filename': filenames,
-                              'predicted_class': classes})
-    labels_df = pd.read_csv(test_labels_path)
-    output_df = output_df.merge(labels_df, left_on='filename', right_on='fname') \
+    output_df = pd.DataFrame({'filename': filenames, 'class': classes})
+    labels_df = pd.read_csv(labels_info_path)
+    output_df = output_df.merge(labels_df, left_on='class', right_on='label') \
                          .drop(columns=['class'])
-    accuracy = len(output_df.loc[output_df['predicted_class'] == output_df['label'], :]) / len(output_df)
-    print(f'Accuracy: {accuracy:.2f}')
+    print(output_df)
 
 
 if __name__ == "__main__":
