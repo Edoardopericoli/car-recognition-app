15ac071ef4246078c16382851cffd6eb4db5c769
edoardopericoli <pericoli.edoardo@gmail.com>
2019-11-13 17:33:29 +0100

    YOLO setting

--- object_detection/move_files.py
+++ object_detection/move_files.py
@@ -1,69 +1,68 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 11 10:13:22 2019
-
-@author: valentinaparietti
-
-Launch from terminal when you are in the folder containing the folders with the objects found for each image
-"""
-
 from PIL import Image
 import shutil
 import os
-#import time
 import re
+import click
 
-#keep only the biggest cut car 
-dirs = filter(os.path.isdir, os.listdir(os.curdir))
-for dir in dirs:
-    files = os.listdir(str(os.curdir)+ str("/") + dir)
-    cars_size = {}
-    
-    for file in files:
-        if not file.startswith('car'):
-#            print(f'\n The image named {file} in the folder {dir} does not start with -car- and so it will be removed \n')
-#            time.sleep(5)
-            os.unlink(str(os.curdir)+ str("/") + dir + str("/") +file)
-    remaining_files = os.listdir(str(os.curdir)+ str("/") + dir)
-    
-    for file in remaining_files:
-        cars_size[file] = Image.open(str(os.curdir)+ str("/") + dir + str("/") +file).size
-    if len(cars_size) > 1:
-#        print(f"In the folder {dir} there are multiple images: {list(cars_size.keys())}: \n")
-        biggest_car = None
-        dim = (0,0)
-        
-        for car in cars_size.keys():
-            if cars_size[car][0] * cars_size[car][1] > dim[0] * dim[1]:
-                biggest_car = car
-                dim = cars_size[car]
-#        print(f"The biggest image is: {biggest_car}: \n")
-        to_delete = (list(set(cars_size.keys())))
-        to_delete.remove(biggest_car)
-#        print(f"The following images: {to_delete} will be removed \n\n")
-#        time.sleep(5)
-        
-        for small_car in to_delete:
-            os.unlink(str(os.curdir)+ str("/") + dir + str("/") +small_car)
-            
-#Rename the images as number.jpg    
-dirs = filter(os.path.isdir, os.listdir(os.curdir))
-for dir in dirs:    
-    files = os.listdir(str(os.curdir)+ str("/") + dir) 
-    for file in files:
-        number = re.search(r"[0-9]+", str(dir))
-        new_name=str(number.group())+".jpg"
-        start = str(os.curdir)+ str("/") + dir + str("/") + file
-        end = str(os.curdir)+ str("/") + dir + str("/") + new_name
-        os.rename(start,end)
- 
-#Put the all the cut cars into a folder named "output images"       
-dirs = filter(os.path.isdir, os.listdir(os.curdir))
-os.mkdir("output_images")
-for dir in dirs:    
-    files = os.listdir(str(os.curdir)+ str("/") + dir) 
-    for file in files:
-        start = str(os.curdir)+ str("/") + dir + str("/") + file 
-        destination = str(os.curdir) + str("/") + "output_images" + str("/") + file
-        shutil.copyfile(start, destination)
+@click.command()
+@click.option('--origin_images_path', default=r"../data/raw_data/cars_train", help='folder from which detecting images',
+              type=str)
+@click.option('--destination_images_path', default=r"../data/object_detection_data/output_images_YOLO",
+              help='folder from which detecting images', type=str)
+
+def main(origin_images_path, destination_images_path):
+
+    # keep only the biggest cut car
+    dirs = filter(os.path.isdir, os.listdir(os.curdir))
+    for dir in dirs:
+        files = os.listdir(str(os.curdir)+ str("/") + dir)
+        cars_size = {}
+
+        for file in files:
+            if not file.startswith('car'):
+                # print(f'\n The image named {file} in the folder {dir}
+                # does not start with -car- and so it will be removed \n')
+
+                # time.sleep(5)
+                os.unlink(str(os.curdir)+ str("/") + dir + str("/") +file)
+        remaining_files = os.listdir(str(os.curdir)+ str("/") + dir)
+
+        for file in remaining_files:
+            cars_size[file] = Image.open(str(os.curdir)+ str("/") + dir + str("/") +file).size
+        if len(cars_size) > 1:
+            # print(f"In the folder {dir} there are multiple images: {list(cars_size.keys())}: \n")
+            biggest_car = None
+            dim = (0,0)
+
+            for car in cars_size.keys():
+                if cars_size[car][0] * cars_size[car][1] > dim[0] * dim[1]:
+                    biggest_car = car
+                    dim = cars_size[car]
+    #        print(f"The biggest image is: {biggest_car}: \n")
+            to_delete = (list(set(cars_size.keys())))
+            to_delete.remove(biggest_car)
+    #        print(f"The following images: {to_delete} will be removed \n\n")
+    #        time.sleep(5)
+
+            for small_car in to_delete:
+                os.unlink(str(os.curdir)+ str("/") + dir + str("/") +small_car)
+    # Rename the images as number.jpg
+    dirs = filter(os.path.isdir, os.listdir(os.curdir))
+    for dir in dirs:
+        files = os.listdir(str(os.curdir)+ str("/") + dir)
+        for file in files:
+            number = re.search(r"[0-9]+", str(dir))
+            new_name=str(number.group())+".jpg"
+            start = str(os.curdir)+ str("/") + dir + str("/") + file
+            end = str(os.curdir)+ str("/") + dir + str("/") + new_name
+            os.rename(start,end)
+
+    # Put the all the cut cars into a folder named "output images"
+    dirs = filter(os.path.isdir, os.listdir(os.curdir))
+    os.mkdir("output_images")
+    for dir in dirs:
+        files = os.listdir(str(os.curdir)+ str("/") + dir)
+        for file in files:
+            start = str(os.curdir)+ str("/") + dir + str("/") + file
+            destination = str(os.curdir) + str("/") + "output_images" + str("/") + file
+            shutil.copyfile(start, destination)
--- object_detection/yolo_script.py
+++ object_detection/yolo_script.py
@@ -1,20 +1,25 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov  7 14:54:59 2019
-
-@author: valentinaparietti
-
-Launch from terminal when you are in the folder containing the images of the cars you want to cut
-"""
-
 from imageai.Detection import ObjectDetection
-detector = ObjectDetection()
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath("yolo.h5")
-detector.loadModel()
-images=["08145.jpg", "08146.jpg", "08147.jpg", "08214.jpg"]
-for i in images:
-    detections = detector.detectObjectsFromImage(input_image=i, output_image_path="new_{}".format(i), extract_detected_objects=True)
-#    print(detections)
+import os
+import click
 
+@click.command()
+@click.option('--origin_images_path', default=r"../data/raw_data/cars_train", help='folder from which detecting images',
+              type=str)
+@click.option('--destination_images_path', default=r"../data/object_detection_data/output_images_YOLO",
+              help='folder from which detecting images', type=str)
+def main(origin_images_path, destination_images_path):
+
+    detector = ObjectDetection()
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath("../data/object_detection_data/YOLO_weights/yolo.h5")
+    detector.loadModel()
+    images = os.listdir(origin_images_path)
+
+    for i in images:
+        detector.detectObjectsFromImage(input_image=i,
+                                        output_image_path=destination_images_path + "/new_{}".format(i),
+                                        extract_detected_objects=True)
+
+
+if __name__ == "__main__":
+    main()
