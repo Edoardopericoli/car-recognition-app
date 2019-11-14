a82290a541afb4a137204472138721205c4197f9
edoardopericoli <pericoli.edoardo@gmail.com>
2019-11-14 11:14:19 +0100

    - added mkdir to splitter and label. - uniformed yolo files (something should be fixed)

--- object_detection/yolo_script.py
+++ object_detection/yolo_script.py
@@ -2,22 +2,28 @@ from imageai.Detection import ObjectDetection
 import os
 import click
 
+
 @click.command()
-@click.option('--origin_images_path', default=r"../data/raw_data/cars_train", help='folder from which detecting images',
+@click.option('--origin_images_path', default=r"../data/raw_data/cars_train/", help='folder from which detecting images',
               type=str)
-@click.option('--destination_images_path', default=r"../data/object_detection_data/output_images_YOLO",
+@click.option('--destination_images_path', default=r"../data/object_detection_data/output_images_YOLO/",
               help='folder from which detecting images', type=str)
-def main(origin_images_path, destination_images_path):
+@click.option('--sample', default=False,
+              help='if True detects only a sample of the images', type=bool)
+def main(origin_images_path, destination_images_path, sample):
 
     detector = ObjectDetection()
     detector.setModelTypeAsYOLOv3()
-    detector.setModelPath("../data/object_detection_data/YOLO_weights/yolo.h5")
+    detector.setModelPath("../data/raw_data/YOLO_weights/yolo.h5")
     detector.loadModel()
     images = os.listdir(origin_images_path)
 
+    if sample:
+        images = images[:10]
+
     for i in images:
-        detector.detectObjectsFromImage(input_image=i,
-                                        output_image_path=destination_images_path + "/new_{}".format(i),
+        detector.detectObjectsFromImage(input_image=origin_images_path + i,
+                                        output_image_path=destination_images_path + "new_{}".format(i),
                                         extract_detected_objects=True)
 
 
