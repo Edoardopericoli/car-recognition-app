8295460361ad10158b9b0ebe72f998120d63924c
edoardomanieri <edoardomanieri@gmail.com>
2019-11-14 17:17:40 +0100

    modified tests

--- unit_test/test_pipeline.py
+++ unit_test/test_pipeline.py
@@ -1,6 +1,9 @@
 from Car_Prediction.models import Prototype
 from Car_Prediction import pipeline
+import os
 
 
 def test_run():
-    pipeline.run(net=Prototype)
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    initial_parameters_path = file_path + "/../config/initial_parameters_test.yml"
+    pipeline.run(initial_parameters_path=initial_parameters_path, net=Prototype)
