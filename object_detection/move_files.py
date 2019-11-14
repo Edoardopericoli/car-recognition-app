752bae437ef6f0fb68f2d16ff4e62aeac039e5ab
edoardopericoli <pericoli.edoardo@gmail.com>
2019-11-14 14:36:37 +0100

    uniformed move_files to directories

--- object_detection/move_files.py
+++ object_detection/move_files.py
@@ -12,19 +12,18 @@ import click
               help='folder from which detecting images', type=str)
 def main(origin_images_path, destination_images_path):
     # Keep only the biggest cut car
-    dirs = filter(os.path.isdir, os.listdir(origin_images_path))
+    dirs = list(filter(os.path.isdir, [origin_images_path + '/' + i for i in os.listdir(origin_images_path)]))
 
     for directory in dirs:
-        files = os.listdir(str(origin_images_path) + str("/") + directory)
+        files = os.listdir(directory)
         cars_size = {}
-
         for file in files:
-            if not file.startswith('car'):
-                os.unlink(str(origin_images_path) + str("/") + directory + str("/") + file)
-        remaining_files = os.listdir(str(origin_images_path) + str("/") + directory)
+            if not (file.startswith('car') or file.startswith('truck')):
+                os.unlink(directory + "/" + file)
+        remaining_files = os.listdir(directory)
 
         for file in remaining_files:
-            cars_size[file] = Image.open(str(origin_images_path) + str("/") + directory + str("/") + file).size
+            cars_size[file] = Image.open(directory + str("/") + file).size
         if len(cars_size) > 1:
             biggest_car = None
             dim = (0, 0)
@@ -38,30 +37,30 @@ def main(origin_images_path, destination_images_path):
             to_delete.remove(biggest_car)
 
             for small_car in to_delete:
-                os.unlink(str(origin_images_path) + str("/") + directory + str("/") + small_car)
+                os.unlink(directory + str("/") + small_car)
 
     # Rename the images as number.jpg
-    dirs = filter(os.path.isdir, os.listdir(origin_images_path))
+    dirs = filter(os.path.isdir, [origin_images_path + '/' + i for i in os.listdir(origin_images_path)])
 
     for directory in dirs:
-        files = os.listdir(str(origin_images_path) + str("/") + directory)
+        files = os.listdir(directory)
         for file in files:
             number = re.search(r"[0-9]+", str(directory))
             new_name = str(number.group())+".jpg"
-            start = str(origin_images_path) + str("/") + directory + str("/") + file
-            end = str(origin_images_path) + str("/") + directory + str("/") + new_name
+            start = directory + str("/") + file
+            end = directory + str("/") + new_name
             os.rename(start, end)
 
-    # Put the all the cut cars into a folder named "output_images_chopped"
-    dirs = filter(os.path.isdir, os.listdir(destination_images_path))
-    if not os.path.exists(destination_images_path + "/output_images_chopped"):
-        os.makedirs(destination_images_path + "/output_images_chopped")
+    # Put the all the cut cars into a folder named "output_images_cropped"
+    dirs = filter(os.path.isdir, [origin_images_path + '/' + i for i in os.listdir(origin_images_path)])
+    if not os.path.exists(destination_images_path + "/output_images_cropped"):
+        os.mkdir(destination_images_path + "/output_images_cropped")
 
     for directory in dirs:
-        files = os.listdir(str(origin_images_path) + str("/") + directory)
+        files = os.listdir(directory)
         for file in files:
-            start = str(origin_images_path) + str("/") + directory + str("/") + file
-            destination = str(destination_images_path + "/output_images_chopped") + str("/") + file
+            start = directory + str("/") + file
+            destination = str(destination_images_path + "/output_images_cropped") + str("/") + file
             shutil.copyfile(start, destination)
 
 
