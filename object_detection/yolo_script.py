d55cdce7b0bd677f6a7c3bf4496d5cef8645da7b
edoardopericoli <pericoli.edoardo@gmail.com>
2019-11-14 11:36:15 +0100

    - added mkdir

--- object_detection/yolo_script.py
+++ object_detection/yolo_script.py
@@ -4,13 +4,16 @@ import click
 
 
 @click.command()
-@click.option('--origin_images_path', default=r"../data/raw_data/cars_train/", help='folder from which detecting images',
+@click.option('--origin_images_path', default=r"../data/raw_data/cars_train", help='folder from which detecting images',
               type=str)
-@click.option('--destination_images_path', default=r"../data/object_detection_data/output_images_YOLO/",
+@click.option('--destination_images_path', default=r"../data/object_detection_data/output_images_YOLO",
               help='folder from which detecting images', type=str)
 @click.option('--sample', default=False,
               help='if True detects only a sample of the images', type=bool)
 def main(origin_images_path, destination_images_path, sample):
+
+    if not os.path.exists(destination_images_path):
+        os.makedirs(destination_images_path)
 
     detector = ObjectDetection()
     detector.setModelTypeAsYOLOv3()
@@ -22,8 +25,8 @@ def main(origin_images_path, destination_images_path, sample):
         images = images[:10]
 
     for i in images:
-        detector.detectObjectsFromImage(input_image=origin_images_path + i,
-                                        output_image_path=destination_images_path + "new_{}".format(i),
+        detector.detectObjectsFromImage(input_image=origin_images_path + '/' + i,
+                                        output_image_path=destination_images_path + "/new_{}".format(i),
                                         extract_detected_objects=True)
 
 
