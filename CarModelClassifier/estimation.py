29c71e44e3016c9bc2c08f413fd6d7b066c4f295
edoardopericoli <pericoli.edoardo@gmail.com>
2019-11-21 14:07:40 +0100

    Fixed dropout

--- CarModelClassifier/estimation.py
+++ CarModelClassifier/estimation.py
@@ -12,7 +12,7 @@ import os
 import glob
 import pandas as pd
 import yaml
-from CarModelClassifier.utils import swish
+from CarModelClassifier.utils import swish, FixedDropout
 
 
 def evaluation(execution_path, test_images_path, test_labels_path):
@@ -21,7 +21,7 @@ def evaluation(execution_path, test_images_path, test_labels_path):
     with open(initial_parameters_path) as f:
         initial_parameters = yaml.load(f)
     model_path = execution_path + '/model.h5'
-    model = load_model(model_path, custom_objects={'swish': swish})
+    model = load_model(model_path, custom_objects={'swish': swish, 'FixedDropout': FixedDropout})
 
     if os.path.isfile(test_images_path):
         img = cv2.imread(test_images_path)
--- CarModelClassifier/utils.py
+++ CarModelClassifier/utils.py
@@ -174,3 +174,13 @@ def save_model_info(username, model, initial_parameters, history):
 def swish(x):
     return K.sigmoid(x)*x
 
+
+def FixedDropout(x):
+    def _get_noise_shape(self, inputs):
+        if self.noise_shape is None:
+            return self.noise_shape
+
+        symbolic_shape = K.shape(inputs)
+        noise_shape = [symbolic_shape[axis] if shape is None else shape
+                       for axis, shape in enumerate(self.noise_shape)]
+        return tuple(noise_shape)
